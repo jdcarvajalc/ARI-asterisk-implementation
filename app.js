1086b4dbe4e2934c6dee1ad4ca67417edf3b1b22
Juan David C <jdcc1308@gmail.com>
2023-05-15 17:38:33 -0500

    Starting project

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/*
+    Instalar dependencias con npm:
+        1. ari-client
+        2. mysql
+        3. google-tts-api
+        4. sox-audio
+*/
+
+const rutaAudioMapu = '/var/lib/asterisk/sounds/custom/citas';
+
+var ari = require('ari-client');
+
+
+ari.connect('http://localhost:8088', 'adminari', '1234', clientLoaded);
+
+
+function clientLoaded(err, client) {
+    if (err) {
+        throw err;
+    }
+
+    /** Listener Start Stasis */
+    client.on('StasisStart', (event, incoming) => {
+
+        // Send a Playback
+        incoming.answer((err, channel) => {
+
+            // Playback
+            play(incoming, 'sound:'+rutaAudioMapu, (err) => {
+                // Playback Completed - Send a Hangup Channel
+                incoming.hangup((err) => {
+                });
+            });
+        });
+    });
+
+
+    /** Function for Playback a Sound */
+    function play(channel, sound, callback) {
+        var playback = client.Playback();
+        playback.once('PlaybackFinished',
+            function (event, instance) {
+                if (callback) {
+                    callback(null);
+                }
+            });
+        channel.play({ media: sound }, playback, (err, playback) => { });
+    }
+
+    client.start('cita-ivr');
+}
